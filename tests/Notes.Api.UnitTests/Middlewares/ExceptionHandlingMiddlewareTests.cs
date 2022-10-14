b371c45c6a3776b971f683389be5057a5b5760f5
Kash.Piasecki <kash.piasecki@protonmail.com>
2022-10-14 12:48:16 +0200

    Add middleware tests

---
+++ tests/Notes.Api.UnitTests/Middlewares/ExceptionHandlingMiddlewareTests.cs
@@ -0,0 +1,92 @@
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Notes.Api.Middlewares;
+using Notes.Application.Common.Exceptions;
+using NUnit.Framework;
+using TddXt.AnyRoot;
+using TddXt.AnyRoot.Strings;
+using static TddXt.AnyRoot.Root;
+
+namespace Notes.Api.UnitTests.Middlewares;
+
+public class ExceptionHandlingMiddlewareTests
+{
+    [Test]
+    public async Task InvokeAsync_CalledWithoutAnError_CallsNext()
+    {
+        // Arrange
+        var response = Any.String();
+        var defaultContext = new DefaultHttpContext
+        {
+            Response =
+            {
+                Body = new MemoryStream()
+            }
+        };
+        var logger = Any.Instance<ILogger<ExceptionHandlingMiddleware>>();
+
+        // Act
+        var middlewareInstance = new ExceptionHandlingMiddleware(logger);
+        await middlewareInstance.InvokeAsync(defaultContext, innerHttpContext =>
+        {
+            innerHttpContext.Response.WriteAsync(response);
+            return Task.CompletedTask;
+        });
+
+        // Assert
+        defaultContext.Response.Body.Seek(0, SeekOrigin.Begin);
+        var body = await new StreamReader(defaultContext.Response.Body).ReadToEndAsync();
+        body.Should().BeEquivalentTo(response);
+    }
+    
+    [Test]
+    public async Task InvokeAsync_CalledWithUnexpectedException_Returns500InternalServerError()
+    {
+        // Arrange
+        var defaultContext = new DefaultHttpContext();
+        var logger = Any.Instance<ILogger<ExceptionHandlingMiddleware>>();
+
+        // Act
+        var middlewareInstance = new ExceptionHandlingMiddleware(logger);
+        var expectedException = Any.Exception();
+        await middlewareInstance.InvokeAsync(defaultContext, _ => Task.FromException(expectedException));
+        // Assert
+        var result = (HttpStatusCode)defaultContext.Response.StatusCode;
+        result.Should().Be(HttpStatusCode.InternalServerError);
+    }
+    
+    [Test]
+    public async Task InvokeAsync_CalledWithNotFoundException_Returns404NotFound()
+    {
+        // Arrange
+        var defaultContext = new DefaultHttpContext();
+        var logger = Any.Instance<ILogger<ExceptionHandlingMiddleware>>();
+
+        // Act
+        var middlewareInstance = new ExceptionHandlingMiddleware(logger);
+        var expectedException = Any.Instance<NotFoundException>();
+        await middlewareInstance.InvokeAsync(defaultContext, _ => Task.FromException(expectedException));
+        // Assert
+        var result = (HttpStatusCode)defaultContext.Response.StatusCode;
+        result.Should().Be(HttpStatusCode.NotFound);
+    }
+    
+    [Test]
+    public async Task InvokeAsync_CalledWithValidationException_Returns422UnprocessableEntity()
+    {
+        // Arrange
+        var defaultContext = new DefaultHttpContext();
+        var logger = Any.Instance<ILogger<ExceptionHandlingMiddleware>>();
+
+        // Act
+        var middlewareInstance = new ExceptionHandlingMiddleware(logger);
+        var expectedException = Any.Instance<ValidationException>();
+        await middlewareInstance.InvokeAsync(defaultContext, _ => Task.FromException(expectedException));
+        // Assert
+        var result = (HttpStatusCode)defaultContext.Response.StatusCode;
+        result.Should().Be(HttpStatusCode.UnprocessableEntity);
+    }
+    
+}
